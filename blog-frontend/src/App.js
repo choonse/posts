1a8aa1aca611a2ff87d9ddff644759e993b09abd
choonse <perfecris@naver.com>
2021-07-19 09:10:27 +0900

    front

---
+++ blog-frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route} from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PostPage from './pages/PostPage';
+import WritePage from './pages/WritePage';
+
+
+
+const App = () => {
+  return (
+    <div>
+    <Route component={PostListPage} path={['/@:username','/']} exact />      
+    <Route component={LoginPage} path="/login" />
+    <Route component={RegisterPage} path="/register" />
+    <Route component={WritePage} path="/write" />
+    <Route component={PostPage} path="/@:username/:postId" />
+    </div>
+  );
+}
+
+export default App;
